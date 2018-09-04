964a0fea86f46efa2d56ee43f2b6969fab8f9243
Maya Flor <maya.r.rebelo@gmail.com>
2018-09-04 00:33:29 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports.cardValidator = function cardValidator (creditCardNumber){
+  if (creditCardNumber === "") {
+    throw new Error ("Type a credit card number");
+  };
+  if (typeof creditCardNumber === "string") {
+    throw new Error ("Type a credit card number");
+  };
+  if (creditCardNumber.length < 2) {
+    throw new Error ("Type a credit card number");
+  };
+  const reversedNumbers = creditCardNumber.split("").reverse();
+
+  let result = 0;
+  for (var i = 0; i < reversedNumbers.length; i++) {
+    if (i % 2 === 0) {
+      result += parseInt(reversedNumbers[i]);
+    } else {
+      product = reversedNumbers[i] * 2;
+      if (product >= 10) {
+        product = product - 9;
+      }
+      result += product;
+    }
+  }
+  
+    if (result % 10 === 0){
+      return true;
+    } else {
+      return false;
+    };
+}
+
+
